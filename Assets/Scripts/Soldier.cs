98141e4cef822f608c04dc929aceb7587b876308
ChrisLubin <chrisx700@gmail.com>
2023-03-02 00:19:40 -0500

    Added gun to player with idle and walking animations

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Soldier : MonoBehaviour
 {
     private Vector3 _targetPosition;
+    private Animator _soldierAnimator;
+    private const string IS_WALKING_ANIMATION_NAME = "IsWalking";
+
+    private void Start()
+    {
+        this._soldierAnimator = GetComponentInChildren<Animator>();
+    }
     
     private void Update()
     {
@@ -17,11 +24,13 @@ public class Soldier : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, this._targetPosition) < 0.05f)
         {
+            _soldierAnimator.SetBool(IS_WALKING_ANIMATION_NAME, false);
             return;
         }
 
-        Vector3 moveDirection = (_targetPosition - transform.position).normalized;
+        _soldierAnimator.SetBool(IS_WALKING_ANIMATION_NAME, true);
         float moveSpeed = 4f;
+        Vector3 moveDirection = (_targetPosition - transform.position).normalized;
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
