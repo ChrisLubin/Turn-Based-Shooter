d4ef120322d10f345b367c331051790b833f179d
ChrisLubin <chrisx700@gmail.com>
2023-03-04 13:15:30 -0500

    Added tilt functionality to camera

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -1,8 +1,32 @@
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private CinemachineTransposer _cinemachineTransposer;
+    private Vector3 _targetFollowOffset;
+    private const float _MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float _MAX_FOLLOW_Y_OFFSET = 12f;
+
+    private void Awake()
+    {
+        CinemachineVirtualCamera cinemachineVirtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+        this._cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    private void Start()
+    {
+        this._targetFollowOffset = this._cinemachineTransposer.m_FollowOffset;
+    }
+
     private void Update()
+    {
+        this.HandleMovement();
+        this.HandlePan();
+        this.HandleTilt();
+    }
+
+    private void HandleMovement()
     {
         Vector3 inputMoveDir = new();
         if (Input.GetKey(KeyCode.W))
@@ -24,7 +48,10 @@ public class CameraController : MonoBehaviour
 
         Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
         transform.position += moveVector * 10f * Time.deltaTime;
+    }
 
+    private void HandlePan()
+    {
         Vector3 rotationVector = new();
         if (Input.GetKey(KeyCode.Q))
         {
@@ -35,5 +62,28 @@ public class CameraController : MonoBehaviour
             rotationVector.y -= 1f;
         }
         transform.eulerAngles += rotationVector * 100f * Time.deltaTime;
+    }
+
+    private void HandleTilt()
+    {
+        float tiltAmount = 1f;
+        float tiltSpeed = 5f;
+        this._cinemachineTransposer.m_FollowOffset = Vector3.Lerp(this._cinemachineTransposer.m_FollowOffset, this._targetFollowOffset, Time.deltaTime * tiltSpeed);
+
+        if (Input.mouseScrollDelta.y == 0)
+        {
+            return;
+        }
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            this._targetFollowOffset.y += tiltAmount;
+        }
+        else
+        {
+            this._targetFollowOffset.y -= tiltAmount;
+        }
+
+        this._targetFollowOffset.y = Mathf.Clamp(this._targetFollowOffset.y, _MIN_FOLLOW_Y_OFFSET, _MAX_FOLLOW_Y_OFFSET);
     }
 }
