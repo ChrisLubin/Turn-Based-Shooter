aa036a6b6de04477b13e2b5cfb2cd5e91d4118a3
ChrisLubin <chrisx700@gmail.com>
2023-03-18 17:02:36 -0400

    Crate explodes into little pieces when it is destroyed

--- Assets/Scripts/Core/Crate.cs
+++ Assets/Scripts/Core/Crate.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
+    [SerializeField] private Transform _destroyedCreatePrefab;
+
     private void OnDestroy()
     {
         Floor.Instance.RebakeFloor();
@@ -9,6 +11,21 @@ public class Crate : MonoBehaviour
 
     public void TakeDamage()
     {
+        Transform destroyedCrate = Instantiate(this._destroyedCreatePrefab, transform.position, transform.rotation);
+        this.ApplyExplosionToRagdoll(destroyedCrate, 150f, transform.position, 10f);
         Destroy(gameObject);
+    }
+
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody))
+            {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+
+            this.ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
